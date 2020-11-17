ea880908cfa5aae45ce3ab1e72d933d74d95ef79
renee212020 <71196944+renee212020@users.noreply.github.com>
2020-11-17 01:17:03 -0500

    Update lab9ex1.js

--- lab9ex1.js
+++ lab9ex1.js
@@ -1,8 +1,11 @@
+//This will output when the page loads
 var start = new Date()
-
 var seconds = newdate.getSeconds();
 
 console.log(seconds);
 
-
+//This will output when a button is pressed
 var stop = new Date()
+var seconds = newdate.getSeconds();
+
+console.log(seconds);
