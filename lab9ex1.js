6782de63125a82d7fee66f34d18720225a4cfd58
renee212020 <71196944+renee212020@users.noreply.github.com>
2020-11-17 03:08:00 -0500

    ex2

--- lab9ex1.js
+++ lab9ex1.js
@@ -1,10 +1,9 @@
-//This will output when the page loads
+
 var start = new Date();
 var seconds = start.getSeconds();
 
 console.log(seconds);
 
-//This will output when a button is pressed
 var stop = new Date();
 
 console.log(stop);
---
+++ lab9ex2.js
@@ -0,0 +1,11 @@
+var myPrompt =prompt("Please enter your age");
+
+console.log(myPrompt);
+
+var newdate = new Date();
+var year = newdate.getFullYear();
+
+var birthyear = year - myprompt;
+
+console.log(year);
+console.log(birthyear);
