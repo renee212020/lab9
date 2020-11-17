2c4873bedc68e870ea9b12a15f0e3c0a61522c7e
renee212020 <71196944+renee212020@users.noreply.github.com>
2020-11-17 02:47:58 -0500

    Update lab9ex1.js

--- lab9ex1.js
+++ lab9ex1.js
@@ -6,7 +6,8 @@ console.log(seconds);
 
 //This will output when a button is pressed
 var stop = new Date();
-var seconds = newdate.getSeconds();
+
+
 document.getElementById("stoptime").innerHTML=result;
 var myConfirm =confirm("Please Confirm");
 
