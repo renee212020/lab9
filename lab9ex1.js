771088ecd68f4b638dab44cd104a8a44cd123c43
renee212020 <71196944+renee212020@users.noreply.github.com>
2020-11-17 00:33:39 -0500

    lab 9

---
+++ lab9ex1.js
@@ -0,0 +1,9 @@
+//This will output when the page loads
+document.getElementById('StopTime').innerHTML=StopTime();
+  var start = new Date();
+
+  //This will output when a button is pressed
+document.getElementById('StopTime').innerHTML=StopTime();
+    var start = new Date.getDate();
+.Confirm("StopTime");
+start.getTime()
---
+++ test.js
@@ -0,0 +1,14 @@
+var newdat = new Date();
+
+var day = newdate.getDate();
+
+var month = newdate.getMonth()+1;
+
+var year = newdate.getFullYear();
+
+var daymonthyear = day + month + year;
+
+console.log(day);
+console.log(month);
+console.log(year);
+console.log(daymonthyear);
